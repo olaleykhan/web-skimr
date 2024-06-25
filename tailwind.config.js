fe10a985fbc26ed56b706ec96cda5aab24b8e92c
olaleykhan <realjoy9@gmail.com>
2024-06-25 07:07:42 +0100

    config

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  mode: "jit",
+  darkMode: "class",
+  content: ["./**/*.tsx"],
+  // theme: {
+  //   extend: {
+  //     colors: {
+  //       gray: {
+  //         900: '#111111', // Vercel's dark gray
+  //         700: '#333333', // Vercel's medium gray
+  //         600: '#555555', // Vercel's light gray for hover state
+  //       },
+  //     },
+  //   },
+  // },
+  plugins: [],
+
+}
